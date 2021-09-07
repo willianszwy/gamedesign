604577a113b858ff3b75045c9eda38396fb1c7a9
willians <willians@iMac-de-willians.local>
2021-09-07 01:02:47 -0400

    atividade 000

---
+++ Atividade000/Terrible Tweeters/Assets/Scripts/LevelController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    [SerializeField] string _nextLevelName;
+
+    Monster[] _monsters;
+     
+
+    void OnEnable()
+    {
+        _monsters = FindObjectsOfType<Monster>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MosterAreAllDead())
+            GoToNextLevel();
+    }
+
+    private void GoToNextLevel()
+    {
+        Debug.Log("Go to level" + _nextLevelName);
+        SceneManager.LoadScene(_nextLevelName);
+    }
+
+    private bool MosterAreAllDead()
+    {
+        foreach (var monster in _monsters)
+        {
+            if (monster.gameObject.activeSelf)
+                return false;
+        }
+
+        return true;
+    }
+}
