b397010595f401eeb68f6a74a9a3913859b4ee95
willians <willians@iMac-de-willians.local>
2021-09-22 18:14:08 -0400

    atividade 003

---
+++ At03/Assets/Scripts/Box.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public float jumpForce;
+    public bool isUp;
+
+    public int health = 5;
+
+    public Animator anim;
+
+
+    private void Update()
+    {
+        if(health <= 0)
+        {
+            Destroy(transform.parent.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            if (isUp)
+            {
+                anim.SetTrigger("hit");
+                health--;
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            }
+            else
+            {
+                anim.SetTrigger("hit");
+                health--;
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, -jumpForce), ForceMode2D.Impulse);
+            }
+            
+        }
+    }
+}
---
+++ At03/Assets/Scripts/Flog.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flog : MonoBehaviour
+{
+    private Rigidbody2D rig;
+    private Animator anim;
+
+    public float speed;
+
+    public Transform rightCol;
+    public Transform leftCol;
+
+    public Transform headPoint;
+
+    private bool colliding;
+
+    public LayerMask layer;
+
+    public BoxCollider2D boxCollider2D;
+    public CircleCollider2D circleCollider2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rig.velocity = new Vector2(speed, rig.velocity.y);
+
+        colliding = Physics2D.Linecast(rightCol.position, leftCol.position, layer);
+
+        if (colliding)
+        {
+            transform.localScale = new Vector2(transform.localScale.x * -1f, transform.localScale.y);
+            speed *= -1f;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            float height = collision.contacts[0].point.y - headPoint.position.y;
+
+            if (height > 0)
+            {
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                speed = 0;
+                anim.SetTrigger("die");
+                boxCollider2D.enabled = false;
+                circleCollider2D.enabled = false;
+                rig.bodyType = RigidbodyType2D.Kinematic;
+                Destroy(gameObject, 0.3f);
+            }else
+            {
+                GameController.instance.ShowGameOver();
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
